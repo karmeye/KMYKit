c6f787cb92b5c6d2d7b7220661b17d1dd19010a0
Karmeye <karmeye@users.noreply.github.com>
2016-03-24 15:13:20 +0200

    Add project

---
+++ KMYKit/KMYKit.h
@@ -0,0 +1,19 @@
+//
+//  KMYKit.h
+//  KMYKit
+//
+//  Created on 24/03/16.
+//  Copyright © 2016 Karmeye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMYKit.
+FOUNDATION_EXPORT double KMYKitVersionNumber;
+
+//! Project version string for KMYKit.
+FOUNDATION_EXPORT const unsigned char KMYKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMYKit/PublicHeader.h>
+
+#import <KMYKit/NSObject+KMY.h>
