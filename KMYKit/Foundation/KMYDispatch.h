6abdb22df32f570dac65bdc45745e73505c64e7b
Karmeye <karmeye@users.noreply.github.com>
2016-12-12 12:35:28 +0300

    Add kmy_dispatch_async_if

--- KMYKit/Foundation/KMYDispatch.h
+++ KMYKit/Foundation/KMYDispatch.h
@@ -6,31 +6,9 @@
 //  Copyright © 2016 Karmeye. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 NS_ASSUME_NONNULL_BEGIN
-
-/// Submits a block for asynchronous execution on a main dispatch queue and returns immediately.
-/// The main dispatch queue is a globally available serial queue that executes tasks on the application’s main thread.
-extern void                 kmy_dispatch_async_on_main_queue(dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on a main dispatch queue after a delay in seconds and returns immediately.
-extern void                 kmy_dispatch_async_on_main_queue_after(NSTimeInterval delay, dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on the given dispatch queue after a delay in seconds and returns immediately.
-extern void                 kmy_dispatch_async_on_queue_after(dispatch_queue_t queue, NSTimeInterval delay, dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on the default priority dispatch queue and returns immediately.
-extern void                 kmy_dispatch_async_on_default_priority_queue(dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on the high priority dispatch queue and returns immediately.
-extern void                 kmy_dispatch_async_on_high_priority_queue(dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on the low priority dispatch queue and returns immediately.
-extern void                 kmy_dispatch_async_on_low_priority_queue(dispatch_block_t block);
-
-/// Submits a block for asynchronous execution on the background priority dispatch queue and returns immediately.
-extern void                 kmy_dispatch_async_on_background_priority_queue(dispatch_block_t block);
 
 /// Returns the serial dispatch queue associated with the application’s main thread.
 #define                     kmy_dispatch_get_main_queue() dispatch_get_main_queue()
@@ -46,5 +24,33 @@ extern dispatch_queue_t     kmy_dispatch_get_low_priority_queue(void);
 
 /// Returns the concurrent global background priority queue, DISPATCH_QUEUE_PRIORITY_BACKGROUND.
 extern dispatch_queue_t     kmy_dispatch_get_background_priority_queue(void);
+
+
+
+/// Submits a block for asynchronous execution on a main dispatch queue and returns immediately.
+/// The main dispatch queue is a globally available serial queue that executes tasks on the application’s main thread.
+extern void                 kmy_dispatch_async_on_main_queue(dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on the default priority dispatch queue and returns immediately.
+extern void                 kmy_dispatch_async_on_default_priority_queue(dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on the high priority dispatch queue and returns immediately.
+extern void                 kmy_dispatch_async_on_high_priority_queue(dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on the low priority dispatch queue and returns immediately.
+extern void                 kmy_dispatch_async_on_low_priority_queue(dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on the background priority dispatch queue and returns immediately.
+extern void                 kmy_dispatch_async_on_background_priority_queue(dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on a main dispatch queue after a delay in seconds and returns immediately.
+extern void                 kmy_dispatch_async_on_main_queue_after(NSTimeInterval delay, dispatch_block_t block);
+
+/// Submits a block for asynchronous execution on the given dispatch queue after a delay in seconds and returns immediately.
+extern void                 kmy_dispatch_async_on_queue_after(dispatch_queue_t queue, NSTimeInterval delay, dispatch_block_t block);
+
+extern void                 kmy_dispatch_async_if(dispatch_queue_t _Nullable queue, dispatch_block_t block);
+
+
 
 NS_ASSUME_NONNULL_END
--- KMYKit/Foundation/KMYDispatch.m
+++ KMYKit/Foundation/KMYDispatch.m
@@ -8,18 +8,26 @@
 
 #import "KMYDispatch.h"
 
+dispatch_queue_t kmy_dispatch_get_default_priority_queue(void) {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+}
+
+dispatch_queue_t kmy_dispatch_get_high_priority_queue(void) {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+}
+
+dispatch_queue_t kmy_dispatch_get_low_priority_queue(void) {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+}
+
+dispatch_queue_t kmy_dispatch_get_background_priority_queue(void) {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+}
+
+#pragma mark -
+
 void kmy_dispatch_async_on_main_queue(dispatch_block_t block) {
     dispatch_async(kmy_dispatch_get_main_queue(), block);
-}
-
-void kmy_dispatch_async_on_main_queue_after(NSTimeInterval delay, dispatch_block_t block) {
-    int64_t delta = (int64_t)(1.0e9 * delay);
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), kmy_dispatch_get_main_queue(), block);
-}
-
-void kmy_dispatch_async_on_queue_after(dispatch_queue_t queue, NSTimeInterval delay, dispatch_block_t block) {
-    int64_t delta = (int64_t)(1.0e9 * delay);
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), queue, block);
 }
 
 void kmy_dispatch_async_on_default_priority_queue(dispatch_block_t block) {
@@ -38,18 +46,20 @@ void kmy_dispatch_async_on_background_priority_queue(dispatch_block_t block) {
     dispatch_async(kmy_dispatch_get_background_priority_queue(), block);
 }
 
-dispatch_queue_t kmy_dispatch_get_default_priority_queue(void) {
-    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+void kmy_dispatch_async_on_main_queue_after(NSTimeInterval delay, dispatch_block_t block) {
+    int64_t delta = (int64_t)(1.0e9 * delay);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), kmy_dispatch_get_main_queue(), block);
 }
 
-dispatch_queue_t kmy_dispatch_get_high_priority_queue(void) {
-    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+void kmy_dispatch_async_on_queue_after(dispatch_queue_t queue, NSTimeInterval delay, dispatch_block_t block) {
+    int64_t delta = (int64_t)(1.0e9 * delay);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), queue, block);
 }
 
-dispatch_queue_t kmy_dispatch_get_low_priority_queue(void) {
-    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+void kmy_dispatch_async_if(dispatch_queue_t queue, dispatch_block_t block) {
+    if (queue)  dispatch_async(queue, block);
+    else        block();
 }
 
-dispatch_queue_t kmy_dispatch_get_background_priority_queue(void) {
-    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
-}
+
+
