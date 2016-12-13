e8dac6e66ded858728725221ba8d1cc7fe8fa024
Karmeye <karmeye@users.noreply.github.com>
2016-12-13 14:48:45 +0300

    Add methods

--- KMYKit/Foundation/KMYBlock.h
+++ KMYKit/Foundation/KMYBlock.h
@@ -6,9 +6,15 @@
 //  Copyright © 2016 Karmeye. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+#import <KMYKit/KMYDispatch.h>
+#import <KMYKit/KMYAssert.h>
 
 ///
 /// See Checking For Null Blocks: http://nshipster.com/new-years-2016/
 ///
-#define KMYInvokeBlockIfSet(block, ...) if (block) { block(__VA_ARGS__); };
+#define KMYInvokeBlockIfSet(block, ...)                             if (block) { block(__VA_ARGS__); };
+
+
+#define KMYInvokeBlockAsyncOnQueueIfSet(queue, block, ...)          if (block) { KMYAssert(queue != NULL); dispatch_async(queue, ^{ block(__VA_ARGS__); }); };
+#define KMYInvokeBlockAsyncOnMainQueueIfSet(block, ...)             if (block) { dispatch_async(dispatch_get_main_queue(), ^{ block(__VA_ARGS__); }); };
