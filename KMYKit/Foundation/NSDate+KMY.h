1f7494fc3c425c9ec5f7c90720cd4b41b930498c
Karmeye <karmeye@users.noreply.github.com>
2017-01-19 09:43:46 +0300

    Add NSDate category

---
+++ KMYKit/Foundation/NSDate+KMY.h
@@ -0,0 +1,22 @@
+//
+//  NSDate+KMY.h
+//  KMYKit
+//
+//  Created on 2017-01-17.
+//  Copyright © 2017 Karmeye. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define KMYTimeIntervalMinute       60
+#define KMYTimeIntervalHour         KMYTimeIntervalMinute   * 60
+#define KMYTimeIntervalDay          KMYTimeIntervalHour     * 24
+#define KMYTimeIntervalWeek         KMYTimeIntervalDay      * 7
+
+@interface NSDate (KMY)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KMYKit/Foundation/NSDate+KMY.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+KMY.m
+//  KMYKit
+//
+//  Created on 2017-01-17.
+//  Copyright © 2017 Karmeye. All rights reserved.
+//
+
+#import "NSDate+KMY.h"
+
+@implementation NSDate (KMY)
+
+@end
