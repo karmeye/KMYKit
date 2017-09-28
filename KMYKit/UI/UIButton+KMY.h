a306501bdee1037ff95cc5b126d8535b224075a3
Karmeye <karmeye@users.noreply.github.com>
2017-09-28 15:26:52 +0300

    Add Swift name

--- KMYKit/UI/UIButton+KMY.h
+++ KMYKit/UI/UIButton+KMY.h
@@ -13,11 +13,11 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIButton (KMY)
 
 + (instancetype)kmy_buttonWithType:(UIButtonType)buttonType
-                       initializer:(void (^_Nullable)(__kindof UIButton *button))initializer;
+                       initializer:(void (^_Nullable)(__kindof UIButton *button))initializer NS_SWIFT_NAME(init(type:initializer:));
 
 + (instancetype)kmy_buttonWithType:(UIButtonType)buttonType
                        initializer:(void (^_Nullable)(__kindof UIButton *button))initializer
-                           handler:(void (^_Nullable)(__kindof UIButton *button))handler;
+                           handler:(void (^_Nullable)(__kindof UIButton *button))handler NS_SWIFT_NAME(init(type:initializer:handler:));
 
 /// May be modified after initialization.
 @property (nonatomic, copy, nullable) void (^kmy_touchUpInsideHandler)(__kindof UIButton *button);
